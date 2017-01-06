baf146fcebb1a316ed9e3df793e354c07f761ce3
Hector Barrios Barrera <hector.barriosbarrera@gmail.com>
2017-01-06 21:08:39 +0000

    Saludo en colores

--- HolaMundo.java
+++ HolaMundo.java
@@ -5,8 +5,8 @@ public class HolaMundo {
 	public static void main(String[] args) {
 	String naranja="\033[33m";
 	String azul="\033[34m";
-	System.out.println("¡Hola Mundo!");
-	System.out.println("Aquí estamos haciendo pruebas con Git");
+	System.out.println(naranja+"¡Hola Mundo!");
+	System.out.println(azul+"Aquí estamos haciendo pruebas con Git");
 	}
 }
 
