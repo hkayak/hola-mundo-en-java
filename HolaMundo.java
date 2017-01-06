8dcad77eb5d0dab72fa5d0632a9c0ab564551f67
Hector Barrios Barrera <hector.barriosbarrera@gmail.com>
2017-01-06 21:05:46 +0000

    Colores Definidos

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,6 +3,8 @@
 */
 public class HolaMundo {
 	public static void main(String[] args) {
+	String naranja="\033[33m";
+	String azul="\033[34m";
 	System.out.println("¡Hola Mundo!");
 	System.out.println("Aquí estamos haciendo pruebas con Git");
 	}
